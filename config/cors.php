f7fe16e985203c82d8ea4672754ff41396e5bd3d
ezzeldeen tantawy <eldeene103@gmail.com>
2025-04-25 18:03:33 +0300

    add cors

--- config/cors.php
+++ config/cors.php
@@ -19,7 +19,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ["https://ezzeldeen's_portfolio.vercel.app"],
+    'allowed_origins' => ["https://ezzeldeenportfolio.vercel.app/"],
 
     'allowed_origins_patterns' => [],
 
